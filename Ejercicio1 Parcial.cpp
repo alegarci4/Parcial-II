c9b9eb3638a342c30794e1d8e094cd64c89712f7
alegarci4 <mgarciah30@miumg.edu.gt>
2024-09-21 10:39:53 -0600

    Add files via upload

---
+++ Ejercicio1 Parcial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int numero;
+	cout << "Ingrese el número a cual desee que llegue su tabla";
+	cin >> numero;
+	
+	cout << "Quiero la tabla del" << numero >> 
+	for (int i = 1
+	
+}
+return 0
+
